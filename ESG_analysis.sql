6c9948005789ed535a0d88c2961fcdb4d6c2369d
1leena <lovleenav@gmail.com>
2026-09-18 00:14:21 +0100

    Add ESG analysis queries for industries and correlation
    Added queries to analyze ESG scores by industry and correlation with profit margin.

--- ESG_analysis.sql
+++ ESG_analysis.sql
@@ -6,3 +6,28 @@ FROM company_esg_financial_dataset
 GROUP BY industry 
 ORDER BY Average_ESG DESC;
 
+
+Query 2 question:
+whcih industries have the highest average environmental ESG score?
+  
+SELECT industry, AVG(ESG_environmental) AS Average_Environmental_ESG
+FROM company_esg_financial_dataset 
+GROUP BY industry 
+ORDER BY Average_Environmental_ESG DESC;
+
+
+Query 3 question:
+is there a relationship between a company ESG performance and its profit margin 
+
+SELECT CORR(Average_ESG, Average_ProfitMargin) AS ESG_Profit_correlation 
+ FROM (
+      SELECT 
+      CompanyName, 
+      AVG(ESG_overall) AS Average_ESG, 
+      AVG(ProfitMargin) AS Average_ProfitMargin 
+      FROM company_esg_financial_dataset 
+      GROUP BY CompanyName
+) AS Company_averages;
+
+
+
