3fa7da3e02b47b2ad9bba997bffef5c4ec00ae5e
1leena <lovleenav@gmail.com>
2026-09-17 23:44:49 +0100

    Add query for average ESG score by industry
    Add query to find industries with highest average ESG score

--- ESG_analysis.sql
+++ ESG_analysis.sql
@@ -1,1 +1,8 @@
+Query 1 question: 
+Which industries have the highest average ESG score? 
+
+SELECT industry, AVG(ESG_overall) AS Average_ESG 
+FROM company_esg_financial_dataset 
+GROUP BY industry 
+ORDER BY Average_ESG DESC;
 
